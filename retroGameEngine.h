9f980a91f09d66be394782897eb0bff618da82e8
jpromanonet <programming@jpromano.net>
2020-05-29 16:36:30 -0600

    Performance update

--- retroGameEngine.h
+++ retroGameEngine.h
@@ -434,7 +434,7 @@ namespace jpr // All OneLoneCoder stuff will now exist in the "jpr" namespace
 		// Common initialisation functions
 		void jpr_UpdateMouse(int32_t x, int32_t y);
 		void jpr_UpdateMouseWheel(int32_t delta);
-		void olc_UpdateWindowSize(int32_t x, int32_t y);
+		void jpr_UpdateWindowSize(int32_t x, int32_t y);
 		void olc_UpdateViewport();
 		bool olc_OpenGLCreate();
 		void olc_ConstructFontSheet();
