08cf134b69f121678a6a80c331c166bffa35f648
jpromanonet <programming@jpromano.net>
2020-05-29 11:38:08 -0600

    Performance update

--- retroGameEngine.h
+++ retroGameEngine.h
@@ -1799,7 +1799,7 @@ namespace jpr
 					{
 						KeySym sym = XLookupKeysym(&xev.xkey, 0);
 						pKeyNewState[mapKeys[sym]] = true;
-						XKeyEvent *e = (XKeyEvent *)&xev; // Because DragonEye loves numpads
+						XKeyEvent *e = (XKeyEvent *)&xev; 
 						XLookupString(e, NULL, 0, &sym, NULL);
 						pKeyNewState[mapKeys[sym]] = true;
 					}
